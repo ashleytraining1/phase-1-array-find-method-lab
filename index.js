8d4e9192cd19aa7151882870b67ee93dfcfb6bc6
ashley <ashley.oketo@student.moringaschool.com>
2022-08-18 18:23:40 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // code your solution here
+function superbowlWin(arrayOfRecords) {
+
+    let myArrayRecords =  arrayOfRecords.find(function (record) {
+        if (record.result === "W"){
+            return true;
+        }else {
+            return false;
+        }
+        
+    })
+ 
+    if(myArrayRecords){
+        return myArrayRecords.year
+    }else{
+        return undefined
+    }
+}
+
